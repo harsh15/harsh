5faa6e411b648f2757b16a05c606e5bb37029e41
Harshvardhan Parihar <harshvardhanparihar@yahoo.com>
2015-09-22 23:06:58 +0530

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   def prepare_meta_tags
     setting_name = "#{params[:controller].gsub("/", ".")}.#{action_name}"
+    val = setting_value("title", setting_name)
+    logger.info("\n\n 11 setting name = #{setting_name}, val = #{val} \n\n")
     set_meta_tags :title => setting_value("title", setting_name),
                   :description => setting_value("meta.description", setting_name),
                   :keywords => setting_value("meta.keywords", setting_name),
